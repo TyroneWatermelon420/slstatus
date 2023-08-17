95f7aafec1d21192dc279c596cbcb6ba7bf2d72e
TyroneWatermelon420 <tyronewatermlon@tutanota.com>
2023-08-17 13:32:44 +0200

    update(smol)

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const struct arg args[] = {
 	//function format          argument
 	//{ wifi_essid, "^b#004b72^^c#d4d4d4^ 󰤨 %s ^d^", "eth0" },
 	{ cpu_perc, "[CPU: %s%%] ", NULL },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ run_command, ":%4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
     { ram_perc, "[ %s%%] ", NULL },
 	{ datetime, "[ %s ", "%a %d %b  %I:%M %p]" },
 };
